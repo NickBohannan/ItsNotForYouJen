e07221d7a435817d3e5d50e2061b428bfb82b32f
Nick Bohannan <nbohannan@gmail.com>
2024-04-28 18:40:39 -0500

    removing superfluous code

--- itsnotforyoujen.js
+++ itsnotforyoujen.js
@@ -2,45 +2,38 @@
 // @name     ItsNotForYouJen
 // @version  1
 // @grant    none
+// @match    https://twitter.com/
 // @match    https://twitter.com/home
 // ==/UserScript==
 
+'use strict';
 
+function hideForYou() {
+  const links = Array.from(document.querySelectorAll('a')).slice(0, 20);
+  links.forEach(link => {
+    const spans = link.querySelectorAll('span');
+    spans.forEach(span => {
+      if (span.innerHTML.trim() === 'For you') {
+        let parentDiv = link.closest('div');
+        if (parentDiv) {
+          parentDiv.style.display = 'none';
+        }
+      }
+      if (span.innerHTML.trim() === 'Following') span.innerHTML = 'lol pwn3d';
+    });
+  });
+}
 
-(function() {
-    'use strict';
-
-    function hideForYou() {
-        const links = Array.from(document.querySelectorAll('a')).slice(0, 20);
-        links.forEach(link => {
-            const spans = link.querySelectorAll('span');
-            spans.forEach(span => {
-                if (span.innerHTML.trim() === 'For you') {
-                    let parentDiv = link.closest('div');
-                    if (parentDiv) {
-                        parentDiv.style.display = 'none';
-                    }
-                }
-            });
-        });
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.addedNodes.length) {
+      hideForYou();
     }
+  });
+});
 
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            if (mutation.addedNodes.length) {
-                hideForYou();
-            }
-        });
-    });
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
-
-    windows.onload = hideForYou;
-
-    window.addEventListener('popstate', function() {
-        hideForYou();
-    });
-})();
